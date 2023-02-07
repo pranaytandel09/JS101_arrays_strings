c49275246a284a2d8e2494967c1dae4b04687a64
pranaytandel09 <pranaytandel09@gmail.com>
2023-02-07 10:34:38 +0530

    gien no is pallindrome or not using string

---
+++ string1.js
@@ -0,0 +1,19 @@
+// given no is a pallindrome or not
+
+let num=1221;
+
+let a= num.toString();
+
+
+ let bag="";
+ for(i=(a.length-1); i>=0; i--){
+     bag=bag+a[i];
+ }
+
+
+  if(a==bag){
+     console.log("Yes");
+ }
+ else{
+     console.log("No");
+ }
